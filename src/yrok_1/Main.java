8980946bcba84db62fd5c905f6bd76ac563d641e
Lebedev <novgrod1123@gmail.com>
2024-05-17 22:47:21 +0300

    add repo to git

---
+++ src/yrok_1/Main.java
@@ -0,0 +1,11 @@
+package yrok_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tree myTree = new Tree();
+        System.out.println(myTree);
+        myTree.cutDown();
+        System.out.println(myTree);
+    }
+}
---
+++ src/yrok_1/Tree.java
@@ -0,0 +1,35 @@
+package yrok_1;
+
+public class Tree {
+    boolean isFruit;
+    int age;
+    double height;
+    String name;
+
+    Tree(){
+      name = "appleTree";
+      age = 15;
+      height = 5.75;
+      isFruit = true;
+    }
+
+    Tree(String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public void cutDown(){
+        this.height = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Tree{" +
+                "isFruit=" + isFruit +
+                ", age=" + age +
+                ", height=" + height +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
