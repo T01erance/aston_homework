e4cf4b9dfc756d35fc5fb2898e91dc5315617345
Lebedev <novgrod1123@gmail.com>
2024-05-19 13:29:02 +0300

    HomeWork_2 task_3

---
+++ src/Homework_2/task_3/Computer.java
@@ -0,0 +1,18 @@
+package Homework_2.task_3;
+
+public class Computer {
+    public static class Processor{
+        String model;
+        public String getDetails(){
+            return model;
+        }
+
+    }
+    class RAM{
+        int size;
+        public int getDetails(){
+            return size;
+        }
+
+    }
+}
---
+++ src/Homework_2/task_3/Test.java
@@ -0,0 +1,7 @@
+package Homework_2.task_3;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
