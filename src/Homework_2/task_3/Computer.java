bee121c9d34b00fdace52773a0fb970fec7d24b5
Lebedev <novgrod1123@gmail.com>
2024-05-19 13:47:25 +0300

    HomeWork_2 task_3

--- src/Homework_2/task_3/Computer.java
+++ src/Homework_2/task_3/Computer.java
@@ -3,9 +3,11 @@ package Homework_2.task_3;
 public class Computer {
     public static class Processor{
         String model;
+
         public String getDetails(){
             return model;
         }
+
 
     }
     class RAM{
--- src/Homework_2/task_3/Test.java
+++ src/Homework_2/task_3/Test.java
@@ -2,6 +2,17 @@ package Homework_2.task_3;
 
 public class Test {
     public static void main(String[] args) {
+        Computer.Processor myProcessor = new Computer.Processor();
+        System.out.println(myProcessor.getDetails());
+        myProcessor.model = "i5";
+        System.out.println(myProcessor.getDetails());
+        Computer myComputer = new Computer();
+
+        Computer.RAM myRAM = myComputer.new RAM();
+        System.out.println(myRAM.getDetails());
+        myRAM.size = 8192;
+        System.out.println(myRAM.getDetails());
+
 
     }
 }
