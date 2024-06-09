ce605a00c754fe3bb2b39613a8b4e92f41985b84
Lebedev <novgrod1123@gmail.com>
2024-06-09 15:27:00 +0300

    dev homework_8 draft

--- src/main/java/homework_8/Main.java
+++ src/main/java/homework_8/Main.java
@@ -3,6 +3,7 @@ package homework_8;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Базовая версия");
+        System.out.println("создаем merge конфликт");
 
     }
 }
